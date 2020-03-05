82d92b908981f0128fb5c91c5e87ce64430fcb2e
zmuranaka <zmuranaka@gmail.com>
2020-03-05 14:41:38 -0700

    Changed from tabs to spaces

--- src/Driver.java
+++ src/Driver.java
@@ -9,37 +9,37 @@ import java.util.Scanner;
 
 public class Driver
 {
-	public static void main(String[] args)
-	{
-		int number = 1;
-		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Enter a positive integer: ");
-		try
-		{
-			number = input.nextInt();
-		}
-		// Catches if the user did not enter a valid int
-		catch(InputMismatchException iException)
-		{
-			System.out.println("Input not valid. Try again.");
-			System.exit(1); // The program ends
-		}
-		
-		try
-		{
-			// Constructs a new FactorialArray object using the number provided
-			FactorialArray array = new FactorialArray(number);
-			System.out.print("The factorial is: ");
-			array.print();
-			System.out.println();
-		}
-		catch(ArrayIndexOutOfBoundsException aiException) // The factorial is more than 100000 digits long
-		{
-			System.out.println("This factorial is too large to be displayed.");
-			System.exit(1); // The program ends
-		}
+    public static void main(String[] args)
+    {
+        int number = 1;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a positive integer: ");
+        try
+        {
+            number = input.nextInt();
+        }
+        // Catches if the user did not enter a valid int
+        catch(InputMismatchException iException)
+        {
+            System.out.println("Input not valid. Try again.");
+            System.exit(1); // The program ends
+        }
+        
+        try
+        {
+            // Constructs a new FactorialArray object using the number provided
+            FactorialArray array = new FactorialArray(number);
+            System.out.print("The factorial is: ");
+            array.print();
+            System.out.println();
+        }
+        catch(ArrayIndexOutOfBoundsException aiException) // The factorial is more than 100000 digits long
+        {
+            System.out.println("This factorial is too large to be displayed.");
+            System.exit(1); // The program ends
+        }
 
-		input.close();
-	}
+        input.close();
+    }
 }
